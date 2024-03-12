078422d6de4b54841fab5f22975153a2c22017b0
Ekaterina <Katya.Kornil10@gmail.com>
2024-03-12 23:34:56 +0300

    change configs

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Profile-task
 ML-инженер
+
+## Чтобы развернуть docker контейнер (docker dekstop уже установлен на пк):
+
+* Через терминал клонируем репозиторий git clone https://github.com/KornilovaK/tg-style-bot.git
+* Из основной директории переходим в docker cd docker
+* Чтобы "построить" контейнер: docker compose -p <название нового контейнера> -f docker-compose.yml build
+* Чтобы запустить его: docker compose -p <название нового контейнера> -f docker-compose.yml up
+
+## Что содержит в себе app/:
+* model.py Основной скрипт работы модели
+* /models Содержит в себе сохраненные веса моделей
+* /data Содержит в себе train_ds.csv, test_ds.csv, EDA.ipynb (предобработка данных), result.txt (результат работы модели - NDCG score для test выборки) и др файлы
--- app/model.py
+++ app/model.py
@@ -1,55 +1,169 @@
 import pandas as pd
 import numpy as np
 import os
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-from sklearn.metrics import roc_auc_score, ndcg_score, accuracy_score, f1_score
+from sklearn.metrics import ndcg_score
 from sklearn.model_selection import train_test_split
-from catboost import CatBoostClassifier
-from sklearn.preprocessing import StandardScaler
+from catboost import CatBoost, Pool
+from dataclasses import dataclass
+from lightgbm import LGBMRanker, Booster
 
-sns.set()
 
-train_df = pd.read_csv('/content/train_df.csv')
-test_df = pd.read_csv('/content/test_df.csv')
+@dataclass
+class Configs:
+    models_path = os.environ.get('MODELS_PATH')
+    data_path = os.environ.get('DATA_PATH')
 
-del_cols = ['feature_0', 'feature_73', 'feature_74', 'feature_75']
-train_df = train_df.drop(del_cols, axis=1)
+    train_df = pd.read_csv(os.path.join(data_path, 'train_df.csv'))
+    test_df = pd.read_csv(os.path.join(data_path, 'test_df.csv'))
+    
+    del_cols = ['feature_3', 'feature_0', 'feature_73',
+                'feature_74', 'feature_75'
+                ]
+    catboost_path = os.path.join(models_path, 'catboost.cbm')
+    lgbm_path = os.path.join(models_path, 'lgbm.txt')
+    res_path = os.path.join(data_path, 'result.txt')
 
-X = train_df.drop(["target"], axis=1)
-y = train_df["target"]
 
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
+def init():
+    configs = Configs()
+    
+    train_df, test_df = preprocess_data(configs)
+    train_catboost(configs, train_df)
+    train_lgbm(configs, train_df)
+    test(configs, test_df)
 
-X_train, x_val, y_train, y_val = train_test_split(X, y,
-                                                  test_size=0.2,
-                                                  random_state=42)
 
-count = y.value_counts()
-weight_0 = count[0]
-weight_1 = count[1]
+def norm(column, eps=1e-6):
+    mean = column.mean()
+    std = column.std()
+    column = (column-mean)/(std+eps)
 
-class_weights = [weight_1/weight_0, weight_0/weight_0]
+    return column
 
-model = CatBoostClassifier(class_weights=class_weights,)
-                           #task_type="GPU")
 
-model.fit(X, y, use_best_model=True, eval_set=(x_val, y_val), plot=True)
+def preprocess_data(configs):
+    train_df, test_df, del_cols = configs.train_df, configs.test_df, configs.del_cols
 
-test_df = test_df.drop(del_cols, axis=1)
+    for col in train_df.columns.values[1:-1]:
+        train_df[col] = norm(train_df[col])
+        test_df[col] = norm(test_df[col])
 
-X_test = test_df.drop(["target"], axis=1)
-y_test = test_df["target"]
+    train_df = train_df.drop(del_cols, axis=1)
+    test_df = test_df.drop(del_cols, axis=1)
 
-scaler = StandardScaler()
-X_test = scaler.fit_transform(X_test)
+    return train_df, test_df
 
-y_pred = model.predict(X_test)
-ndcg_score([y_test], [y_pred])
-model.save_model('last_model.cbm')
 
+def catboost_data(X, y):
+    X_train, x_val, y_train, y_val = train_test_split(X, y,
+                                                    test_size=0.2,
+                                                    random_state=42)
+
+    train = X_train
+    train['target'] = y_train
+    train = train.sort_values(by=['search_id'])
+
+    qids_train = train.search_id.values
+    X_train = train.drop(["search_id", "target"], axis=1)
+    y_train = train['target']
+
+    val = x_val
+    val['target'] = y_val
+    val = val.sort_values(by=['search_id'])
+
+    qids_val = val.search_id.values
+    x_val = val.drop(["search_id", "target"], axis=1)
+    y_val = val['target']
+
+    catboost_pool_train = Pool(X_train, y_train, group_id=qids_train)
+    catboost_pool_test = Pool(x_val, y_val, group_id=qids_val)
+
+    return catboost_pool_train, catboost_pool_test
+
+
+def train_args(train_df):
+    X = train_df.drop(["target"], axis=1)
+    y = train_df["target"]
+
+    count = y.value_counts()
+    weight_0 = count[0]
+    weight_1 = count[1]
+    class_weights = [weight_1/weight_0, weight_0/weight_0]
+
+    return X, y, class_weights
+
+
+def train_catboost(configs, train_df):
+    X, y, class_weights = train_args(train_df)
+
+    catboost_pool_train, catboost_pool_test = catboost_data(X, y)
+
+    catboost_model = CatBoost({
+                        'loss_function':'Logloss',
+                        'learning_rate': 0.01,
+                        'iterations': 1000,
+                        'early_stopping_rounds': 300,
+                        'eval_metric': 'NDCG',
+                        'class_weights':class_weights,
+                        })
+    catboost_model.fit(catboost_pool_train,
+                        eval_set=catboost_pool_test,
+                        use_best_model=True
+                        )
+
+    catboost_model.save_model(configs.catboost_path)
+
+
+def lgbm_data(X, y):
+    X_train, x_val, y_train, y_val = train_test_split(X, y,
+                                                    test_size=0.2,
+                                                    random_state=42)
+
+    qids_train = X_train.groupby("search_id")["search_id"].count().to_numpy()
+    X_train = X_train.drop(["search_id"], axis=1)
+
+    qids_validation = x_val.groupby("search_id")["search_id"].count().to_numpy()
+    x_val = x_val.drop(["search_id"], axis=1)
+
+    return X_train, y_train, qids_train, x_val, y_val, qids_validation
+
+
+def train_lgbm(configs, train_df):
+    X, y, class_weights = train_args(train_df)
+    X_train, y_train, qids_train, x_val, y_val, qids_validation = lgbm_data(X, y)
+
+    model = LGBMRanker(metric="ndcg", n_estimators=1000, learning_rate=0.01, class_weights=class_weights)
+
+    model.fit(
+            X=X_train,
+            y=y_train,
+            group=qids_train,
+            eval_set=[(x_val, y_val)],
+            eval_group=[qids_validation],
+            eval_at=[1, 2, 5, 10],
+            )
+    
+    model.booster_.save_model(configs.lgbm_path)
+
+
+def test(configs, test_df):
+    X_test = test_df.drop(["target", "search_id"], axis=1)
+    y_test = test_df["target"]
+    
+    catboost = CatBoost()
+    catboost.load_model(configs.catboost_path)
+    cat_pred = catboost.predict(X_test)
+    ndcg_cat = ndcg_score([y_test], [cat_pred])
+
+    lgbm = Booster(model_file=configs.lgbm_path)
+    lgmb_pred = lgbm.predict(X_test)
+    ndcg_lgbm = ndcg_score([y_test], [lgmb_pred])
+
+    text = f'NDCG CatBoost score: {ndcg_cat}\nNDCG LightGBM score: {ndcg_lgbm}\n'
+    file = open(configs.res_path, 'w')
+    file.write(text)
+    file.close()
 
 if __name__ == '__main__':
-	dp.run_polling(bot)
+	init()
